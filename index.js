b657dda3404f5c9122c45f6d81ee8f9e9534de5d
taibislamdipu <mailtaibislam@gmail.com>
2023-04-12 16:09:21 +0600

    next question fn add

--- index.js
+++ index.js
@@ -1,4 +1,47 @@
-fetch("quiz.json")
-  .then((res) => res.json())
-  .then((data) => console.log(data))
-  .catch((err) => console.error(err));
+function nextQuestion() {
+  currentQuestion++;
+  console.log("Current question:", currentQuestion);
+  displayQuestion();
+}
+
+var currentQuestion = 0;
+var quizData = [
+  {
+    question: "Question 1",
+    options: ["Option 1 fdfsdff", "Option 2sdfsfsf", "Option 3", "Option 4"],
+    correct_option: 2,
+  },
+  {
+    question: "Question 2",
+    options: ["Option 1 sdfsdf", "Option 2sdfsff", "Option 3sdfsf", "Option 4"],
+    correct_option: 0,
+  },
+  {
+    question: "Question 3",
+    options: ["Option 1lkjkl;", "Option 2kl;kl", "Option 3", "Option 4"],
+    correct_option: 1,
+  },
+  {
+    question: "Question 4",
+    options: ["Option 1", "Option 2", "Option 3", "Option 4"],
+    correct_option: 3,
+  },
+  {
+    question: "Question 5",
+    options: ["Option 1", "Option 2", "Option 3", "Option 4"],
+    correct_option: 2,
+  },
+];
+
+function displayQuestion() {
+  var questionText = quizData[currentQuestion].question;
+  var options = quizData[currentQuestion].options;
+
+  document.getElementById("question").innerHTML = questionText;
+  for (var i = 0; i < options.length; i++) {
+    var optionText = options[i];
+    document.getElementById("option" + i).innerHTML = optionText;
+  }
+}
+
+displayQuestion();
